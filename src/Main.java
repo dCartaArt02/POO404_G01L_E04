9251674de837e6d8ed84ef51d0d12c37931275a9
Luis <ca241136@alumno.udb.edu.sv>
2024-07-29 14:53:22 -0600

    avance2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.swing.*;
+
+public class Main extends JFrame
+{
+    public Main()
+    {
+        JLabel hola = new JLabel("Hola");
+        add(hola);
+        this.setSize(200,300);
+        this.setTitle("JFRAME");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+    public static void main(String[] args)
+    {
+        new Main();
+
+
+
     }
 }
