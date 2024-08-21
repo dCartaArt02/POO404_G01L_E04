ed1bafae2006bfea5cd9adacd251ee0ce59dbebf
Carta <ca241136@alumno.udb.edu.sv>
2024-08-21 01:25:14 -0600

    avance_fechaDeNacimiento

---
+++ Ca241136_Ejercicio _1/src/utilidades/Fecha_nacimiento.java
@@ -0,0 +1,32 @@
+package utilidades;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+public class Fecha_nacimiento {
+    private String Fechacumple;
+
+        private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        public long calcularEdad(String fechaNacimiento) {
+            try {
+                LocalDate fecha = LocalDate.parse(fechaNacimiento, formatter);
+                LocalDate hoy = LocalDate.now();
+                return ChronoUnit.YEARS.between(fecha, hoy);
+            } catch (Exception e) {
+
+                throw new IllegalArgumentException("Formato de fecha incorrecto. Use dd/MM/yyyy.");
+            }
+
+
+            public int obteneranio (String fechanacimiento)
+            {
+                try {
+                    LocalDate fecha
+
+                } catch ()
+
+            }
+        }
+
+}
--- Ca241136_Ejercicio _1/src/utilidades/ingresoDatos.java
+++ Ca241136_Ejercicio _1/src/utilidades/ingresoDatos.java
@@ -25,7 +25,7 @@ public class ingresoDatos extends JFrame {
         this.setLocationRelativeTo(null);
         combosexo.addItem("Masculino");
         combosexo.addItem("Femenino");
-       
+        
 
         saludarButton.addActionListener(new ActionListener() {
             @Override
@@ -35,7 +35,24 @@ public class ingresoDatos extends JFrame {
                 String sexo = (String) combosexo.getSelectedItem();
                 String fechanacimientoCD = DDMMAATextField.getText();
 
-              
+                try {
+
+                    Fecha_nacimiento Calcu = new Fecha_nacimiento();
+                    long edad = Calcu.calcularEdad(fechanacimientoCD);
+                   
+                    
+                        JOptionPane.showMessageDialog(null, "Hola!  " + nombre + " Tienes la edad de! " + edad + " Años y sexo!  " + sexo + " Un Saludo! " +bici);
+                    
+
+                }
+                catch (IllegalArgumentException ex)
+                {
+                    JOptionPane.showMessageDialog(null, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                }
+
+
+            }
+        });
     }
 
         public static void main (String[]args){
