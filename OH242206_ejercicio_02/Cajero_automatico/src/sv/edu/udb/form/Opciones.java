5bcc96275a5bc49d9624e74d391ec8f9de34a644
LuisOrt1z <OH242206@alumno.udb.edu.sv>
2024-08-21 09:54:44 -0600

    segundo commit
    aqui solo avance en la  creacion y funcionalidad de un radiobutton :P

--- OH242206_ejercicio_02/Cajero_automatico/src/sv/edu/udb/form/Opciones.java
+++ OH242206_ejercicio_02/Cajero_automatico/src/sv/edu/udb/form/Opciones.java
@@ -57,30 +57,81 @@ public class Opciones extends JFrame {
         rbt_retiro.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                
+
+                if (rbt_retiro.isSelected()) {
+                    String cantidadstring = JOptionPane.showInputDialog(null,"Ingrese la cantidad a retirar:");
+
+                    if(cantidadstring != null){
+
+                        try{
+                            String Saldo = cuentasRecibidas[cuentaseleccionadaRecibida][1];
+                            double saldoint = Double.parseDouble(Saldo);
+
+                            double cantidad = Double.parseDouble(cantidadstring);
+                            double temp;
+                            if(cantidad<=saldoint){
+                                temp = saldoint-cantidad;
+                                cuentasRecibidas[cuentaseleccionadaRecibida][1].toString();
+                                JOptionPane.showMessageDialog(null,"Se han retirado [$"+cantidad+"] su saldo actual es [$"+temp+"]",
+                                        "RETIRO EXITOSO", JOptionPane.INFORMATION_MESSAGE);
+                                rbt_retiro.setSelected(false);
+                            }else {
+                                JOptionPane.showMessageDialog(null,"Fondos insuficientes","ADVERTENCIA",JOptionPane.WARNING_MESSAGE);
+                                rbt_retiro.setSelected(false);
+                            }
+
+                        }catch (NumberFormatException exception){
+                            JOptionPane.showMessageDialog(null, "El campo no es valido", "ERROR", JOptionPane.ERROR_MESSAGE);
+                            rbt_retiro.setSelected(false);
+                        }
+                    }
+                }
 
             }
         });
-
 
         rbt_consignar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                if(rbt_consignar.isSelected()){
+                 String cantidad = JOptionPane.showInputDialog(null,"Ingrese la cantidad a consignar:");
+
+                 if(cantidad != null){
+                     try{
+
+                         String saldo = cuentasRecibidas[cuentaseleccionadaRecibida][1];
+                         double saldodouble = Double.parseDouble(saldo);
+
+
+                         double cantidaddouble = Double.parseDouble(cantidad);
+                         if(cantidaddouble<=saldodouble){
+                             double tempsaldofinal = saldodouble+cantidaddouble;
+                             JOptionPane.showMessageDialog(null,"Se han consignado [$"+cantidaddouble+"] su saldo actual es [$"+tempsaldofinal);
+                            rbt_consignar.setSelected(false);
+                         }else {
+                             JOptionPane.showMessageDialog(null,"Fondos insuficientes","ERROR",JOptionPane.ERROR_MESSAGE);
+                             rbt_consignar.setSelected(false);
+                         }
+
+                     }catch (NumberFormatException exception){
+                         JOptionPane.showMessageDialog(null,"Los datos ingresados no son validos","ERROR",JOptionPane.ERROR_MESSAGE);
+                        rbt_consignar.setSelected(false);
+                     }
+                 }else{
+                     JOptionPane.showMessageDialog(null,"El campo esta vacio","ADVERTENCIA",JOptionPane.WARNING_MESSAGE);
+                 }
+                }
             }
         });
 
-
+        
         rbt_transferir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-
             }
         });
-
-
-
     }
 }
 
