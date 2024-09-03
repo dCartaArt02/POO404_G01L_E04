2108b3c313fc34bd89ff302c4d880e45520757d1
Carta <ca241136@alumno.udb.edu.sv>
2024-09-02 21:38:02 -0600

    Avance_Sobre_las_conecion_Sql

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,7 @@
-import javax.swing.*;
-
-public class Main extends JFrame
-{
-    public Main()
-    {
-        JLabel hola = new JLabel("Hola");
-        add(hola);
-        this.setSize(200,300);
-        this.setTitle("JFRAME");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
-    }
-    public static void main(String[] args)
-    {
-        new Main();
-
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
 
     }
 }
